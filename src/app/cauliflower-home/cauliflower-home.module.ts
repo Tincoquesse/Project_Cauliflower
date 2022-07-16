67ed1a5bda3ecbb7eea1ef8cbb243781985635e3
Tincoquesse <kamil.sound@gmail.com>
2022-07-16 13:27:56 +0200

    navbar template added

---
+++ src/app/cauliflower-home/cauliflower-home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CauliflowerHomePageComponent } from './pages/cauliflower-home-page/cauliflower-home-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    CauliflowerHomePageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CauliflowerHomeModule { }
---
+++ src/app/cauliflower-home/pages/cauliflower-home-page/cauliflower-home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cauliflower-home-page',
+  templateUrl: './cauliflower-home-page.component.html',
+  styleUrls: ['./cauliflower-home-page.component.css']
+})
+export class CauliflowerHomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
