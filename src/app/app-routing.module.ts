b2b4ec5b29d4e6d96f4cda4abcf857d61becf96f
Tincoquesse <kamil.sound@gmail.com>
2022-07-16 13:25:51 +0200

    navbar template added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  CauliflowerOfferPageComponent
+} from "./cauliflower-offer/pages/cauliflower-offer-page/cauliflower-offer-page.component";
+import {RouterModule, Routes} from "@angular/router";
+import {
+  CauliflowerHomePageComponent
+} from "./cauliflower-home/pages/cauliflower-home-page/cauliflower-home-page.component";
 
 export const enum RoutesConfig {
-  homePage= 'home',
-  offerPage = 'cauliflower-page',
+  homePage= 'home-page',
+  offerPage = 'cauliflower-offer',
 
 }
 
+const routes: Routes = [
+  {
+    path: RoutesConfig.homePage,
+    component: CauliflowerHomePageComponent,
+  },
+  {
+    path: RoutesConfig.offerPage,
+    component: CauliflowerOfferPageComponent
+  },
+  {path: '', redirectTo: RoutesConfig.homePage, pathMatch: 'full'},
+
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class AppRoutingModule { }
