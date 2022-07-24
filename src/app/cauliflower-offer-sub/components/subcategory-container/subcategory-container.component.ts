3c8d272fb0e4b2068fcf6919f8649bf0e64953d8
PawelGajec <gajec.pawel@gmail.com>
2022-07-24 12:06:45 +0200

    Add checkbox

--- src/app/cauliflower-offer-sub/components/subcategory-container/subcategory-container.component.ts
+++ src/app/cauliflower-offer-sub/components/subcategory-container/subcategory-container.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Subcategory} from "../../../api/model/Subcategory";
 
 @Component({
   selector: 'app-subcategory-container',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubcategoryContainerComponent implements OnInit {
 
-  constructor() { }
+  @Input() subcategory: Subcategory | undefined;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
