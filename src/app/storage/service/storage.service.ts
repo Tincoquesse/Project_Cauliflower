4a896a97604c6907e4977f05ed8b6b14800b5116
Tincoquesse <kamil.sound@gmail.com>
2022-08-28 11:38:39 +0200

    endpoints addresses changed

---
+++ src/app/storage/service/storage.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {Training} from "../../api/model/Training";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private _trainingOrderList = new BehaviorSubject<Training[]>([]);
+  readonly trainingOrderList = this._trainingOrderList.asObservable();
+
+  constructor() {
+  }
+
+  isContainTraining = (training: Training | undefined): boolean => {
+    if (training !== undefined){
+      return this._trainingOrderList.getValue().includes(training);
+    }else{
+      return false
+    }
+  }
+
+addTrainings = (training: Training) => {
+  if (!this._trainingOrderList.getValue().includes(training)) {
+    this._trainingOrderList.next([...this._trainingOrderList.getValue(), training]);
+  } else {
+    this._trainingOrderList.next(this._trainingOrderList.getValue().filter(e => e.name !== training.name));
+  }
+  console.log(this._trainingOrderList);
+}
+}
