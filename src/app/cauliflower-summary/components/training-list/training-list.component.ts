90632eb86f106baf53c08a629c8bd441babef489
macbookpro <kamil.sound@gmaIl.com>
2022-09-18 11:16:03 +0200

    Kamil's changes

--- src/app/cauliflower-summary/components/training-list/training-list.component.ts
+++ src/app/cauliflower-summary/components/training-list/training-list.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StorageService} from "../../../storage/service/storage.service";
 import {Training} from "../../../api/model/Training";
 import {Observable} from "rxjs";
 import {FormControl, Validators} from "@angular/forms";
+import {CauliflowerService} from "../../../api/service/cauliflower.service";
+import {Router, RouterLink} from "@angular/router";
+import {RoutesConfig} from "../../../app-routing.module";
+import {Offer} from "../../../api/model/Offer";
 
 @Component({
   selector: 'app-training-list',
@@ -14,13 +18,22 @@ export class TrainingListComponent implements OnInit {
   public trainings: Observable<Training[]> | undefined;
   email = new FormControl('', [Validators.email]);
 
-  constructor(private storageService: StorageService) { }
+  constructor(private storageService: StorageService, private service: CauliflowerService, private router: Router) { }
 
   ngOnInit(): void {
     this.trainings = this.storageService.trainingOrderList;
   }
 
-  onSubmit() { //TODO
+  onSubmit() {
+    if (this.email.valid){
+      // let offer: Offer = {
+      //
+      // }
+      // this.service.sendOffer()
+      this.router.navigateByUrl(RoutesConfig.homePage)
+    }
 
   }
+  hasEmailValidateError = (): boolean =>
+    !!this.email.errors?.hasOwnProperty('email');
 }
