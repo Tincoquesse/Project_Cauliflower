cc3026bddd0ff90db6fad6dac55627612c27f5e5
PawelGajec <gajec.pawel@gmail.com>
2022-09-25 10:21:42 +0200

    add end page functionality

--- src/app/cauliflower-end-page/cauliflower-end-page/cauliflower-end-page.component.ts
+++ src/app/cauliflower-end-page/cauliflower-end-page/cauliflower-end-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RoutesConfig} from "../../app-routing.module";
+import {interval, Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-cauliflower-end-page',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CauliflowerEndPageComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  timeLeft: number = 5;
+
+  private timeSubscription: Subscription | undefined;
+
+  constructor(private router: Router) {
+
   }
 
+  ngOnInit(): void {
+    this.timeSubscription = interval(1000).subscribe(() => {
+      this.timeLeft -= 1;
+      if (this.timeLeft < 0) {
+        this.router.navigate([RoutesConfig.homePage])
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.timeSubscription?.unsubscribe();
+  }
 }
