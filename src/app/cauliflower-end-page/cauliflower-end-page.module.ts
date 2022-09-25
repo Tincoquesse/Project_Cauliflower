c2d74f85d1786ee64dd120b7be4442eb53797c41
PawelGajec <gajec.pawel@gmail.com>
2022-09-25 08:37:24 +0200

    add end page

---
+++ src/app/cauliflower-end-page/cauliflower-end-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CauliflowerEndPageComponent } from './cauliflower-end-page/cauliflower-end-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    CauliflowerEndPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CauliflowerEndPageModule { }
---
+++ src/app/cauliflower-end-page/cauliflower-end-page/cauliflower-end-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cauliflower-end-page',
+  templateUrl: './cauliflower-end-page.component.html',
+  styleUrls: ['./cauliflower-end-page.component.css']
+})
+export class CauliflowerEndPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
